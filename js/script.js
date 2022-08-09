e9f8aa77dcdd4ea166f13d6fc2390cba2d4a07e4
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-08-09 12:23:56 +0000

    js_lesson8

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".the Target").skippr({
+  transition: 'fade',
+  speed: 1000,
+  easing: 'easeOutQuart'
+  navType: 'block',
+  childrenElementType: 'div',
+  arrows: true,
+  autoPlay: false,
+  autoPlayDuration: 5000,
+  keyboardOnAlways: true,
+  hidePrevious: false,
+});
